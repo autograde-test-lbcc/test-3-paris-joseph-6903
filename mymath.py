9c4c2a44945b9ee0572b9dd4e9d3bb73180b3cba
Joe Paris <parisj@linnbenton.edu>
2020-03-15 19:54:49 -0700

    Fix one formatting issue

--- mymath.py
+++ mymath.py
@@ -4,6 +4,6 @@ def add(x, y):
     """Compute the sum of x and y."""
     return x+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y-y+y
 
-def subtract( x,  y ):
+def subtract( x, y ):
     """Compute the difference of x and y."""
     return x/y
